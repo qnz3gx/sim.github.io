a71e116fa05f1de24a28b2c55abe88e715ca8b64
Scarlett Morse <scarlettimorse@d-172-18-121-195.vpn.net.virginia.edu>
2025-06-04 11:35:11 -0400

    Data labels, plot

--- data.py
+++ data.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 
 def import_csv_with_pandas(file_path,r):
     data = pd.read_csv(file_path, skiprows=r)
     return data
 
 file_path = '/Users/scarlettimorse/PycharmProjects/PythonProject/Website/Available_Polarized_Data.csv'
-polarized_3he_df = import_csv_with_pandas(file_path,10)
+APD_df = import_csv_with_pandas(file_path,10)
 
 experiment_mapping = {
     1: 'SLAC E142',
@@ -21,63 +20,14 @@ experiment_mapping = {
     8: 'E94010',
 }
 
-polarized_3he_df['Experiment'] = polarized_3he_df['Experiment'].replace(experiment_mapping)
-dash_df = polarized_3he_df.replace(-1000.0, '-')
-
-experiment_groups = {
-    experiment: df for experiment, df in dash_df.groupby('Experiment')
+Q2_groups = {
+    q2: df for q2, df in APD_df.groupby('Q2')
 }
 
-cleaned_experiment_groups = {}
+unique_q2=sorted(APD_df['Q2'].unique())
+unique_q2.remove(-1000.0)
 
-for experiment, df in experiment_groups.items():
-    cleaned_df = df.loc[:, ~(df == '-').any()]
-    cleaned_experiment_groups[experiment] = cleaned_df
-    cleaned_experiment_groups[experiment].iloc[:, :-1].to_csv(f'{experiment}.csv', index=False)
-
-# unique_q2_3 = sorted(cleaned_experiment_groups['Zheng']['Q2'].unique())
-# fig, axs = plt.subplots(1, 3, figsize=(6, 4), sharex=True, sharey=True)
-# axs = axs.flatten()
-
-# for i, q2 in enumerate(unique_q2_3[:3]):
-#     ax = axs[i]
-#     subset = cleaned_experiment_groups['Zheng'][cleaned_experiment_groups['Zheng']['Q2'] == q2]
-#     ax.errorbar(subset['W'], subset['G1.mes'], yerr=subset['G1.mes.err'], fmt='o-', label='G1', capsize=3, elinewidth=0.8)
-#     ax.errorbar(subset['W'], subset['G2.mes'], yerr=subset['G2.mes.err'], fmt='o-', label='G2', capsize=3, elinewidth=0.8)
-#     ax.errorbar(subset['W'], subset['G1F1.mes'], yerr=subset['G1F1.mes.err'], fmt='o-', label='G1F1', capsize=3, elinewidth=0.8)
-#     ax.errorbar(subset['W'], subset['G2F1.mes'], yerr=subset['G2F1.mes.err'], fmt='o-', label='G2F1', capsize=3, elinewidth=0.8)
-#     ax.set_title(f'Q² = {q2}')
-#     ax.set_xlabel('W')
-#     ax.set_ylabel('Measured Quantities')
-#     ax.legend()
-
-# fig.suptitle('Measured Quantities vs W for different Q² values (Zheng)')
-# plt.tight_layout()
-# plt.savefig("Zheng.png")
-# plt.show()
-
-# flay_df = cleaned_experiment_groups['Flay'].copy()
-# flay_df.loc[:, 'Q2'] = pd.to_numeric(flay_df['Q2'], errors='coerce').round()
-# cleaned_experiment_groups['Flay'] = flay_df
-# mostly_unique = cleaned_experiment_groups['Flay']['Q2'].round()
-# unique_q2_5 = sorted(mostly_unique.unique())
-
-# fig, axs = plt.subplots(3, 2, figsize=(12, 8), sharex=True, sharey=True)
-# axs = axs.flatten()
-
-# for i, q2 in enumerate(unique_q2_5[:6]):
-#     ax = axs[i]
-#     subset = cleaned_experiment_groups['Flay'][cleaned_experiment_groups['Flay']['Q2'] == q2]
-#     ax.scatter(subset['W'], subset['G1F1.mes'], label='G1F1')
-#     ax.errorbar(subset['W'], subset['G1F1.mes'], yerr=subset['G1F1.mes.err'], capsize=3, elinewidth=0.8, linestyle='none')
-#     ax.scatter(subset['W'], subset['G2F1.mes'], label='G2F1')
-#     ax.errorbar(subset['W'], subset['G2F1.mes'], yerr=subset['G2F1.mes.err'], capsize=3, elinewidth=0.8, linestyle='none')
-#     ax.set_title(f'Q² \u2248 {q2}')
-#     ax.set_xlabel('W')
-#     ax.set_ylabel('Measured Quantities')
-#     ax.legend()
-
-# fig.suptitle('Measured Quantities vs W for different Q² values (Flay)')
-# plt.tight_layout()
-# plt.savefig("Flay.png")
-# plt.show()
+N = 5
+fig = px.scatter(Q2_groups[unique_q2[N]][(Q2_groups[unique_q2[N]].W > -999) & (Q2_groups[unique_q2[N]]['G1.mes'] > -999)], x='W', y='G1.mes', title='G1 vs W')
+fig.write_html("plotly_figure.html")
+print(unique_q2[N])
