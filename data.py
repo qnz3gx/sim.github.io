ffc8c10c1d6a3211b19b7aac2b959f1e9d6483e2
qnz3gx <qnz3gx@virginia.edu>
2025-06-11 15:25:22 -0400

    plot

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
+from scipy.stats import linregress
 
 def import_csv_with_pandas(file_path,r):
     data = pd.read_csv(file_path, skiprows=r)
@@ -8,6 +11,9 @@ def import_csv_with_pandas(file_path,r):
 
 file_path = '/Users/scarlettimorse/PycharmProjects/PythonProject/Website/Available_Polarized_Data.csv'
 APD_df = import_csv_with_pandas(file_path,10)
+
+columns_to_check = ['X', 'Q2', 'G1.mes']
+h_df = APD_df[~APD_df[columns_to_check].isin([-1000]).any(axis=1)]
 
 experiment_mapping = {
     1: 'SLAC E142',
@@ -20,14 +26,95 @@ experiment_mapping = {
     8: 'E94010',
 }
 
-Q2_groups = {
-    q2: df for q2, df in APD_df.groupby('Q2')
-}
+h_df['Experiment'] = h_df['Experiment'].replace(experiment_mapping)
 
-unique_q2=sorted(APD_df['Q2'].unique())
-unique_q2.remove(-1000.0)
+centers = np.array([0.0036, 0.0045, 0.0055, 0.007, 0.009, 0.012, 0.017, 0.024,
+                    0.035, 0.049, 0.077, 0.12, 0.17, 0.22, 0.29, 0.41, 0.57, 0.74])
 
-N = 5
-fig = px.scatter(Q2_groups[unique_q2[N]][(Q2_groups[unique_q2[N]].W > -999) & (Q2_groups[unique_q2[N]]['G1.mes'] > -999)], x='W', y='G1.mes', title='G1 vs W')
-fig.write_html("plotly_figure.html")
-print(unique_q2[N])
+midpoints = (centers[:-1] + centers[1:]) / 2
+min_x = h_df['X'].min()
+max_x = h_df['X'].max()
+
+edges = np.concatenate([
+    [min(min_x, centers[0] - (centers[1] - centers[0]) / 2)],
+    midpoints,
+    [max(max_x, centers[-1] + (centers[-1] - centers[-2]) / 2)]
+])
+
+bin_labels = np.arange(len(centers))
+
+h_df = h_df.copy()
+h_df.loc[:, 'X_index'] = pd.cut(h_df['X'], bins=edges, labels=False, include_lowest=True)
+
+h_df = h_df.dropna(subset=['X_index'])
+h_df.loc[:, 'X_index'] = h_df['X_index'].astype(int)
+
+plot_df = h_df.copy()
+plot_df['G1(x,Q2)'] = plot_df['G1.mes'] + 12.1 - 0.7 * plot_df['X_index']
+
+fig = go.Figure()
+
+experiments = plot_df['Experiment'].unique()
+bins = sorted(plot_df['X_index'].unique())
+annotations = []
+
+for exp in experiments:
+    exp_df = plot_df[plot_df['Experiment'] == exp]
+    fig.add_trace(go.Scatter(
+        x=exp_df['Q2'],
+        y=exp_df['G1(x,Q2)'],
+        mode='markers',
+        name=str(exp),
+        marker=dict(size=6),
+        legendgroup=str(exp),
+        showlegend=True
+    ))
+
+for bin_idx in bins:
+    bin_df = plot_df[plot_df['X_index'] == bin_idx].sort_values(by='Q2')
+    
+    if len(bin_df) > 1:
+        slope, intercept, _, _, _ = linregress(bin_df['Q2'], bin_df['G1(x,Q2)'])
+        
+        line_x = bin_df['Q2']
+        line_y = slope * line_x + intercept
+        
+        fig.add_trace(go.Scatter(
+            x=line_x,
+            y=line_y,
+            mode='lines',
+            line=dict(color='gray', width=1, dash='solid'),
+            name=f'Best fit - X bin {bin_idx}',
+            legendgroup=f'bin_{bin_idx}',
+            showlegend=False
+        ))
+
+        x_label = line_x.iloc[-1]
+        y_label = line_y.iloc[-1]
+    
+    elif len(bin_df) == 1:
+        x_label = bin_df['Q2'].iloc[0]
+        y_label = bin_df['G1(x,Q2)'].iloc[0]
+
+    else:
+        continue
+
+    annotations.append(dict(
+        x=x_label,
+        y=y_label,
+        text=f"x = {centers[bin_idx]:.4f}",
+        showarrow=False,
+        xshift=40,
+        yshift=0,
+        font=dict(size=10, color="black"),
+        ))
+
+fig.update_layout(
+    title='g\u2081<sup><sup>3</sup>He</sup>(x,Q²) vs Q²',
+    xaxis_title='Q² (GeV²)',
+    yaxis_title='g\u2081<sup><sup>3</sup>He</sup>(x,Q²) + 12.1 - 0.7i',
+    template='plotly_white',
+    annotations=annotations
+)
+
+fig.write_html("g1(3He)_vs_Q2.html")
