32cfa53cd532a9651dcbcbd715e06ab5991c6359
qnz3gx <qnz3gx@virginia.edu>
2025-06-19 14:59:28 -0400

    everything except proton, neutron, compass, g1/f1 neutron

--- G1F1toG1.py
+++ G1F1toG1.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-dataset = 'neutron_SLAC_E155'
+dataset = 'neutron_Flay'
 skip=0
 
 #upload data to dataframes
@@ -14,30 +14,53 @@ def import_csv(file_path, lines):
 
 grid_path = "/Users/scarlettimorse/PycharmProjects/sim.github.io/problemchild.csv"
 G1_path = f"/Users/scarlettimorse/PycharmProjects/sim.github.io/{dataset}.csv"
-grid_df = import_csv(grid_path, 0)
+grid = import_csv(grid_path, 0)
 data_df = import_csv(G1_path, skip)
-data_df = data_df[~data_df['G1F1.mes'].isnull()]
+data_df = data_df[~data_df['g1/F1'].isnull()]
+print(data_df.dtypes)
+print(data_df[['x', 'Q2']].head())
+print(data_df[['x', 'Q2']].isnull().sum())
 # %%
 G1calc = []
 G1errcalc = []
+G1syscalc = []
 for i in range(len(data_df)):
-    target_X = data_df['X'].iloc[i]
+    target_X = data_df['x'].iloc[i]
     target_Q2 = data_df['Q2'].iloc[i]
+
+    grid_df = grid.dropna(subset=['X', 'Q2', 'F1_IpQE'])
+    if grid_df.empty:
+        print(f"Warning: grid_df has no valid rows at index {i}")
+        G1calc.append(np.nan)
+        G1errcalc.append(np.nan)
+        G1syscalc.append(np.nan)
+        continue
     
     # Compute distances from all points in grid_df
     distances = np.sqrt((grid_df['X'] - target_X)**2 + (grid_df['Q2'] - target_Q2)**2)
-    nearest_idx = distances.idxmin()
     
+    if distances.isnull().all():
+        print(f"Warning: All distances are NaN at index {i}")
+        G1calc.append(np.nan)
+        G1errcalc.append(np.nan)
+        G1syscalc.append(np.nan)
+        continue
+
+    nearest_idx = distances.idxmin()
+
     # Get the corresponding F1_IpQE value
     F1_IpQE_value = grid_df.loc[nearest_idx, 'F1_IpQE']
-    G1calc.append(data_df['G1F1.mes'].iloc[i] * F1_IpQE_value)
-    G1errcalc.append(data_df['G1F1.mes.err'].iloc[i] * F1_IpQE_value)
-    print(f"F1_IpQE = {F1_IpQE_value}")
-    print(f"G1F1.mes = {data_df['G1F1.mes'].iloc[i]}")
-    print(f"Grid row:\n{nearest_idx}")
-    print("-" * 50)
+    G1calc.append(data_df['g1/F1'].iloc[i] * F1_IpQE_value)
+    G1errcalc.append(data_df['dg1/F1(stat)'].iloc[i] * F1_IpQE_value)
+    G1syscalc.append(data_df['dg1/F1(sys)'].iloc[i] * F1_IpQE_value)
 
-data_df['G1.mes'] = G1calc
-data_df['G1.mes.err'] = G1errcalc
+data_df['g1'] = G1calc
+data_df['dg1(stat)'] = G1errcalc
+data_df['dg1(sys)'] = G1syscalc
+data_df['dg1(tot)'] = np.sqrt(data_df['dg1(stat)']**2 + data_df['dg1(sys)']**2)
+data_df.round(4)
+
+print(data_df['g1'].head())
 # %%
-data_df.to_csv(f'{dataset}.csv')
+data_df.to_csv(f'{dataset}.csv', index=False)
+# %%
--- unpublishedData.py
+++ unpublishedData.py
@@ -2,110 +2,33 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-# E94010_df = pd.read_csv('allData.csv')
-
-# for i in range(len(E94010_df)):
-#     if E94010_df['Experiment'].iloc[i] == 'E94010':
-#         E94010_df['Q2'].iloc[i] = ''
-#         E94010_df['X'].iloc[i] = ''
-#         E94010_df['W'].iloc[i] = ''
-#         E94010_df['Theta'].iloc[i] = ''
-#         E94010_df['Eb'].iloc[i] = ''
-#         E94010_df['nu'].iloc[i] = ''
-#         E94010_df['G1.mes'].iloc[i] = ''
-#         E94010_df['G1.mes.err'].iloc[i] = ''
-#         E94010_df['G2.mes'].iloc[i] = ''
-#         E94010_df['G2.mes.err'].iloc[i] = ''
-#         E94010_df['G1F1.mes'].iloc[i] = ''
-#         E94010_df['G1F1.mes.err'].iloc[i] = ''
-#         E94010_df['G2F1.mes'].iloc[i] = ''
-#         E94010_df['G2F1.mes.err'].iloc[i] = ''
-
-# E94010_df.to_csv("publishedData.csv", index=False)
-
-# nd_df = pd.read_csv('NeutronData.csv')
-# nd_df = nd_df.iloc[74:108]
-# nd_df = nd_df.dropna(axis=1, how='all')
-# nd_df.to_csv('neutron_SLAC_E155.csv')
-
-# E155df = pd.read_csv('neutron_SLAC_E155.csv')
-
-# nd_df.loc[84:107, 'G1.mes'] = E155df['G1.mes'].values
-# nd_df.loc[84:107, 'G1.mes.err'] = E155df['G1.mes.err'].values
-
-# nd_df.to_csv('NeutronData.csv')
-
 helium = pd.read_csv("threeHedata.csv")
-neutron = pd.read_csv("NeutronData.csv")
+
 def add_column(df,existing,new):
     insert_index = df.columns.get_loc(existing)
     df.insert(loc=insert_index, column=new, value=np.nan)
 
-# add_column(neutron,'g2','dg1(sys)')
-# add_column(neutron,'g2','dg1(tot)')
-# add_column(neutron,'g1/F1','dg2(sys)')
-# add_column(neutron,'g1/F1','dg2(tot)')
-# add_column(neutron,'g2/F1','dg1/F1(sys)')
-# add_column(neutron,'g2/F1','dg1/F1(tot)')
-# add_column(neutron,'sigma_LT','dg2/F1(sys)')
-# add_column(neutron,'sigma_LT','dg2/F1(tot)')
-# add_column(neutron,'sigma_TT','dsigma_LT(sys)')
-# add_column(neutron,'sigma_TT','dsigma_LT(tot)')
-# add_column(neutron,'Experiment','dsigma_TT(sys)')
-# add_column(neutron,'Experiment','dsigma_TT(tot)')
-# helium.drop(helium.columns[0], axis=1, inplace=True)
-# helium.replace(-1000.0,'', inplace=True)
+def quadrature_sum(df, col1, col2, result, experiments):
+    mask = df['Experiment'].isin(experiments)
+    col1_numeric = pd.to_numeric(df.loc[mask, col1], errors='coerce')
+    col2_numeric = pd.to_numeric(df.loc[mask, col2], errors='coerce')
+    df.loc[mask, result] = np.sqrt(col1_numeric**2 + col2_numeric**2)
 
-# add_column(neutron,'g2/F1','A1')
-# add_column(neutron,'g2/F1','dA1(stat)')
-# add_column(neutron,'g2/F1','dA1(sys)')
-# add_column(neutron,'g2/F1','dA1(tot)')
+def sort(df):
+    for experiment in df['Experiment'].unique():
+        df_subset = df[df['Experiment'] == experiment]
+        df_subset = df_subset.dropna(axis=1, how='all')
+        df_subset = df_subset.iloc[:, :-1]
+        filename = f"neutron_{experiment}.csv" #don't forget to change this
+        df_subset.to_csv(filename, index=False)
 
-experiment_mapping = {
-    1: 'SLAC E142',
-    2: 'SLAC E154',
-    3: 'Zheng',
-    4: 'Kramer',
-    5: 'Flay',
-    6: 'Solvignon',
-    7: 'E97110',
-    8: 'E94010',
-}
+def replace_exp(main_df, exp_df, exp):
+    common_cols = main_df.columns.intersection(exp_df.columns)
+    main_df.loc[3:20, common_cols] = exp_df[common_cols].values   
+    print(f"Replaced columns: {list(common_cols)}")
+    return main_df
 
-# mask = helium['Experiment'] == 'SLAC E142'
-# helium.loc[mask, ['dg1(stat)', 'dg1(tot)']] = helium.loc[mask, ['dg1(tot)', 'dg1(stat)']].values
-
-# mask2 = helium['Experiment'] == 'E97110'
-# helium.loc[mask2, ['dg1(stat)', 'dg1(tot)']] = helium.loc[mask2, ['dg1(tot)', 'dg1(stat)']].values
-
-# mask3 = helium['Experiment'] == 'E94010'
-# helium.loc[mask3, ['dg1(stat)', 'dg1(tot)']] = helium.loc[mask3, ['dg1(tot)', 'dg1(stat)']].values
-
-mask = helium['Experiment'] == 'E97110'
-helium.loc[mask, ['dg2(stat)', 'dg2(tot)']] = helium.loc[mask, ['dg2(tot)', 'dg2(stat)']].values
-
-mask2 = helium['Experiment'] == 'E94010'
-helium.loc[mask2, ['dg2(stat)', 'dg2(tot)']] = helium.loc[mask2, ['dg2(tot)', 'dg2(stat)']].values
-helium.loc[mask2, ['dsigma_LT(stat)', 'dsigma_LT(tot)']] = helium.loc[mask2, ['dsigma_LT(tot)', 'dsigma_LT(stat)']].values
-helium.loc[mask2, ['dsigma_TT(stat)', 'dsigma_TT(tot)']] = helium.loc[mask2, ['dsigma_TT(tot)', 'dsigma_TT(stat)']].values
-
-
-# def quadrature_sum(df, col1, col2, result, experiments):
-#     mask = df['Experiment'].isin(experiments)
-#     col1_numeric = pd.to_numeric(df.loc[mask, col1], errors='coerce')
-#     col2_numeric = pd.to_numeric(df.loc[mask, col2], errors='coerce')
-#     df.loc[mask, result] = np.sqrt(col1_numeric**2 + col2_numeric**2)
-
-# experiments_to_update = ['Zheng']
-# quadrature_sum(helium, 'dg1(stat)', 'dg1(sys)', 'dg1(tot)', experiments_to_update)
-# print(helium['dg1(tot)'].sample(20))
-# helium = helium.round(4)
-helium.to_csv("threeHedata.csv", index=False)
-
-for experiment in helium['Experiment'].unique():
-    df_subset = helium[helium['Experiment'] == experiment]
-    df_subset = df_subset.dropna(axis=1, how='all')
-    df_subset = df_subset.iloc[:, :-1]
-    filename = f"{experiment}.csv"
-    df_subset.to_csv(filename, index=False)
-
+def quadrature_sum(df,col1,col2,result):
+    mask = df[col1].notna() & df[col2].notna()
+    df.loc[mask, result] = np.sqrt(df.loc[mask, col1]**2 + df.loc[mask, col2]**2)
+    return df
