524ca1b591fa30e9bded25fd3413eb0f5a6c1149
qnz3gx <qnz3gx@virginia.edu>
2025-06-24 08:54:55 -0400

    hessian error prop

--- gridchecker.py
+++ gridchecker.py
@@ -3,16 +3,21 @@ import pandas as pd
 import numpy as np
 # %%
 # Load tables
-# tableD = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/deu_F1.csv") #CJ15nlo
+tableD = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/deu_F1.csv") #CJ15nlo
 # tableN = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CJ15nlon_F1.csv") #CJ15nlo
 # tableP = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CJ15nlop_F1.csv")
+F1d_err = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/d_err.csv")
+# F1n_err = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CJn_err.csv")
+# F1p_err = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CJp_err.csv")
 
-# tableN = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CT18NNLOn_F1.csv") #CT18NNLO
-# tableP = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CT18NNLOp_F1.csv")
+tableN = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CT18NNLOn_F1.csv") #CT18NNLO
+tableP = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CT18NNLOp_F1.csv")
+F1n_err = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CTn_err.csv")
+F1p_err = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/CTp_err.csv")
 
-tableD = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/table_D_sm3_ipol1_ipolres1_IA14_SF23_AC11_mod1.out") #Zheng
-tableN = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/table_N_sm0_ipol1_ipolres1_IA14_SF23_AC11_mod1.out")
-tableP = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/table_P_sm0_ipol1_ipolres1_IA14_SF23_AC11_mod1.out")
+# tableD = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/table_D_sm3_ipol1_ipolres1_IA14_SF23_AC11_mod1.out") #Zheng
+# tableN = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/table_N_sm0_ipol1_ipolres1_IA14_SF23_AC11_mod1.out")
+# tableP = pd.read_csv("/Users/scarlettimorse/PycharmProjects/PDFs/table_P_sm0_ipol1_ipolres1_IA14_SF23_AC11_mod1.out")
 
 CompassP_path = "/Users/scarlettimorse/PycharmProjects/sim.github.io/CompassProton.csv"
 CompassD_path = "/Users/scarlettimorse/PycharmProjects/sim.github.io/CompassDeuteron.csv"
@@ -74,15 +79,18 @@ def retrieve_g1(data_df,grid_df):
 #Get the F1, G1, and G1 error values for the deuteron
 CompassD['Q2'] = CompassP['Q2'].values
 CompassD['F1'] = retrieve_f1(CompassD,tableD)
+CompassD['F1.err'] = F1d_err['dF1(tot)']
 CompassD['G1'] = CompassD['A1'] * CompassD['F1']
-CompassD['G1.err'] = CompassD['A1.err'] * CompassD['F1']
-CompassD['g1.err'] = CompassD['a1.err'] * CompassD['F1']
+#error for division: g1*sqrt((a1err/a1)^2 + (f1err/f1)^2)
+CompassD['G1.err'] = CompassD['G1'] * np.sqrt((CompassD['A1.err'].values/CompassD['A1'].values)**2 + (CompassD['F1.err'].values/CompassD['F1'].values)**2)
+CompassD['g1.err'] = CompassD['G1'] * np.sqrt((CompassD['a1.err'].values/CompassD['A1'].values)**2 + (CompassD['F1.err'].values/CompassD['F1'].values)**2)
 
 #Get F1, G1, and G1 error values for the proton
 CompassP['F1'] = retrieve_f1(CompassP,tableP)
+CompassP['F1.err'] = F1p_err['dF1(tot)']
 CompassP['G1'] = CompassP['A1'] * CompassP['F1']
-CompassP['G1.err'] = CompassP['A1.err'] * CompassP['F1']
-CompassP['g1.err'] = CompassP['a1.err'] * CompassP['F1']
+CompassP['G1.err'] = CompassP['G1'] * np.sqrt((CompassP['A1.err'].values/CompassP['A1'].values)**2 + (CompassP['F1.err'].values/CompassP['F1'].values)**2)
+CompassP['g1.err'] = CompassP['G1'] * np.sqrt((CompassP['a1.err'].values/CompassP['A1'].values)**2 + (CompassP['F1.err'].values/CompassP['F1'].values)**2)
 
 #Create a neutron dataframe
 neutron_COMPASS = pd.DataFrame()
@@ -121,12 +129,13 @@ spreadsheet['G1n'] = neutron_COMPASS['G1.mes']
 spreadsheet['dG1n(stat)'] = neutron_COMPASS['G1.mes.err']
 spreadsheet['dG1n(sys)'] = neutron_COMPASS['g1.mes.err']
 spreadsheet['F1n'] = retrieve_f1(neutron_COMPASS,tableN)
-spreadsheet['A1n'] = spreadsheet['G1n']/spreadsheet['F1n']
-spreadsheet['dA1n(stat)'] = spreadsheet['dG1n(stat)']/spreadsheet['F1n']
-spreadsheet['dA1n(sys)'] = spreadsheet['dG1n(sys)']/spreadsheet['F1n']
+spreadsheet['dF1n(tot)'] = F1n_err['dF1(tot)']
+spreadsheet['g1/F1n'] = spreadsheet['G1n']/spreadsheet['F1n']
+spreadsheet['dg1/F1n(stat)'] = spreadsheet['g1/F1n'] * np.sqrt((spreadsheet['dG1n(stat)'].values/spreadsheet['G1n'].values) ** 2 + (spreadsheet['dF1n(tot)'].values/spreadsheet['F1n'].values) ** 2)#spreadsheet['dG1n(stat)']/spreadsheet['F1n']
+spreadsheet['dg1/F1n(sys)'] = spreadsheet['dG1n(sys)']/spreadsheet['F1n']
 
 spreadsheet = spreadsheet.round(4)
-spreadsheet.to_csv('COMPASS_recalculated_Zheng.csv',index=False)
+spreadsheet.to_csv('COMPASS_recalculated_CT18NNLO.csv',index=False)
 # %%
 # export separate neutron df
 neutron = pd.DataFrame()
@@ -136,10 +145,11 @@ neutron['g1'] = neutron_COMPASS['G1.mes']
 neutron['dg1(stat)'] = neutron_COMPASS['G1.mes.err']
 neutron['dg1(sys)'] = neutron_COMPASS['g1.mes.err']
 neutron['F1'] = retrieve_f1(neutron_COMPASS,tableN)
+neutron['dF1(tot)'] = spreadsheet['dF1n(tot)']
 neutron['g1/F1'] = spreadsheet['G1n']/spreadsheet['F1n']
 neutron['dg1/F1(stat)'] = spreadsheet['dG1n(stat)']/spreadsheet['F1n']
 neutron['dg1/F1(sys)'] = spreadsheet['dG1n(sys)']/spreadsheet['F1n']
 
 neutron = neutron.round(4)
-neutron.to_csv('neutron_COMPASS.csv',index=False)
+neutron.to_csv('neutron_CT18NNLO.csv',index=False)
 # %%
